630201d6b5bdf2594dbf5e273b8d7900a3f8202d
Alina Otten <alina.otten@de.pwc.com>
2020-06-06 16:57:21 +0200

    get local data and parse into images into key,value pairs for categorizing dog breeds

--- main.py
+++ main.py
@@ -1,43 +1,101 @@
 import tensorflow as tf
 
-tf.keras.backend.set_floatx('float64')
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-mnist = tf.keras.datasets.mnist
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pathlib
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# set directory of training images
+data_dir = "Dog_Breed_Recognition_Competition_Datasets/Dog_Breed_Training_Images"
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
+data_dir = pathlib.Path(data_dir)
 
-predictions = model(x_train[:1]).numpy()
-predictions
+# get possible breed names
+CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
+# print(CLASS_NAMES)
 
-tf.nn.softmax(predictions).numpy()
+# define parameters
+BATCH_SIZE = 32
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# create dataset of file paths
+list_ds = tf.data.Dataset.list_files(str(data_dir / '*/*.jpg'))
 
-loss_fn(y_train[:1], predictions).numpy()
+# for f in list_ds.take(5):
+#     print(f.numpy())
 
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+# convert file path to an (img, label) pair
+def get_label(file_path):
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # The second to last is the class-directory
+    return parts[-2] == CLASS_NAMES
 
-model.fit(x_train, y_train, epochs=5)
+def decode_img(img):
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize the image to the desired size.
+    return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
 
-model.evaluate(x_test,  y_test, verbose=2)
-
-probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
-
-probability_model(x_test[:5])
+def process_path(file_path):
+    label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
 
 
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in labeled_ds.take(1):
+    print("Image shape: ", image.numpy().shape)
+    print("Label: ", label.numpy())
 
 
+def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
+    # # This is a small dataset, only load it once, and keep it in memory.
+    # # use `.cache(filename)` to cache preprocessing work for datasets that don't
+    # # fit in memory.
+    # if cache:
+    #     if isinstance(cache, str):
+    #         ds = ds.cache(cache)
+    #     else:
+    #         ds = ds.cache()
+
+    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+
+    # Repeat forever
+    ds = ds.repeat()
+
+    ds = ds.batch(BATCH_SIZE)
+
+    # `prefetch` lets the dataset fetch batches in the background while the model
+    # is training.
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+
+    return ds
+
+
+train_ds = prepare_for_training(labeled_ds)
+
+image_batch, label_batch = next(iter(train_ds))
+
+
+def show_batch(image_batch, label_batch):
+    plt.figure(figsize=(10, 10))
+    for n in range(5):
+        ax = plt.subplot(5, 5, n + 1)
+        plt.imshow(image_batch[n])
+        plt.title(CLASS_NAMES[label_batch[n] == 1][0].title())
+        plt.axis('off')
+
+    return plt.show()
+
+# show results
+show_batch(image_batch.numpy(), label_batch.numpy())
