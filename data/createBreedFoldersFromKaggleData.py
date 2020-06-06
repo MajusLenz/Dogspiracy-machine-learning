4c2293c045bf1fd8fbf843ec63ec59c7430937c2
Majus Lenz <mariuslenzing@yahoo.de>
2020-06-06 17:19:27 +0200

    added "create breeds folders"-script

--- data/createBreedFoldersFromKaggleData.py
+++ data/createBreedFoldersFromKaggleData.py
@@ -13,38 +13,46 @@ new_test_dir = "images/test"
 kaggle_train_dir = "kaggle/images/Dog_Breed_Training_Images"
 kaggle_test_dir = "kaggle/images/Dog_Breed_Test_Images"
 
-# change vars to sort the test-images too:
-csv_file_path = csv_file_path_train
-new_image_dir = new_train_dir
-old_image_dir = kaggle_train_dir
-
 
 def create_folder_if_not_exists(folder_name):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
 
-with open(csv_file_path, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in csvreader:
-        breed = row[1]
+def create_breed_folders_for_one_csv_file(csv_file_path, new_image_dir, old_image_dir):
+    first_line = True
 
-        # react to possible commas in breed names
-        second_value = row[2]
-        second_value_is_still_breed = breed.find('"', 0) == 0
-        if second_value_is_still_breed:
-            breed = breed + second_value
-        breed = breed.replace('"', '')
+    with open(csv_file_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csvreader:
+            if first_line:
+                first_line = False
+                continue
 
-        create_folder_if_not_exists(new_image_dir + "/" + breed)
+            breed = row[1]
 
+            # react to possible commas in breed names
+            second_value = row[2]
+            second_value_is_still_breed = breed.find('"', 0) == 0
+            if second_value_is_still_breed:
+                breed = breed + second_value
+            breed = breed.replace('"', '')
 
-        if row[0] == "10004890":
+            create_folder_if_not_exists(new_image_dir + "/" + breed)
+
             # copy image to new dir
-            dog_id = row[0]
-            file_name = dog_id + ".jpg"
-            file_path = old_image_dir +  "/" + file_name
-            destination = new_image_dir + "/" + breed + "/" + file_name
-            copyfile(file_path, destination)
+            try:
+                dog_id = row[0]
+                file_name = dog_id + ".jpg"
+                file_path = old_image_dir + "/" + file_name
+                destination = new_image_dir + "/" + breed + "/" + file_name
+                copyfile(file_path, destination)
+            except FileNotFoundError:
+                pass
 
-        # print(breed)
+
+# execute for training data
+create_breed_folders_for_one_csv_file(csv_file_path_train, new_train_dir, kaggle_train_dir)
+
+# execute for test data
+#create_breed_folders_for_one_csv_file(csv_file_path_test, new_test_dir, kaggle_test_dir)
