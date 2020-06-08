ea4361310ec1dc60668328387725b25514171c44
Majus Lenz <mariuslenzing@yahoo.de>
2020-06-08 17:47:03 +0200

    updated TODOs

--- RandomAgent.py
+++ RandomAgent.py
@@ -6,6 +6,7 @@ class RandomAgent:
     def __init__(self, breeds):
         self.breeds = breeds
 
-    def sample(self):
+    # TODO define image type (numpy array??)
+    def sample(self, image):
         random_breed_index = random.randint(0, len(self.breeds) -1)
         return self.breeds[random_breed_index]
