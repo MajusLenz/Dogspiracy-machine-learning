1fe3637f5f413d3a28cb551c95654e56fc08d8db
Alina Otten <alina.otten@de.pwc.com>
2020-06-09 11:41:04 +0200

    add variables test data

--- GetDataReadyForTraining.py
+++ GetDataReadyForTraining.py
@@ -8,15 +8,19 @@ import os
 import pathlib
 import config as cfg
 
-
 # set directory of training images
-train_dir = cfg.train_dir
+train_data_dir = cfg.train_dir
 
-data_dir = pathlib.Path(train_dir)
+# set directory of test images
+test_data_dir = cfg.test_dir
+
+# data directory for preparing data (either train or test data)
+# data_dir = pathlib.Path(train_data_dir)
+data_dir = pathlib.Path(test_data_dir)
 
 # get possible breed names
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
-# print(CLASS_NAMES)
+print('breed names: ', CLASS_NAMES)
 
 # define parameters
 BATCH_SIZE = cfg.batch_size
@@ -25,9 +29,9 @@ IMG_WIDTH = cfg.img_width
 
 # create dataset of file paths
 list_ds = tf.data.Dataset.list_files(str(data_dir / '*/*.jpg'))
+for f in list_ds.take(5):
+    print('image path: ', f.numpy())
 
-# for f in list_ds.take(5):
-#     print(f.numpy())
 
 # convert file path to an (img, label) pair
 def get_label(file_path):
@@ -36,6 +40,7 @@ def get_label(file_path):
     # The second to last is the class-directory
     return parts[-2] == CLASS_NAMES
 
+
 def decode_img(img):
     # convert the compressed string to a 3D uint8 tensor
     img = tf.image.decode_jpeg(img, channels=3)
@@ -43,6 +48,7 @@ def decode_img(img):
     img = tf.image.convert_image_dtype(img, tf.float32)
     # resize the image to the desired size.
     return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
+
 
 def process_path(file_path):
     label = get_label(file_path)
@@ -54,41 +60,41 @@ def process_path(file_path):
 
 # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
 labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
 for image, label in labeled_ds.take(1):
     print("Image shape: ", image.numpy().shape)
     print("Label: ", label.numpy())
 
 
 def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
-    # # This is a small dataset, only load it once, and keep it in memory.
-    # # use `.cache(filename)` to cache preprocessing work for datasets that don't
-    # # fit in memory.
     # if cache:
     #     if isinstance(cache, str):
     #         ds = ds.cache(cache)
     #     else:
     #         ds = ds.cache()
 
+    # allocate buffer
     ds = ds.shuffle(buffer_size=shuffle_buffer_size)
 
-    # Repeat forever
+    # re-initialize the dataset
     ds = ds.repeat()
 
     ds = ds.batch(BATCH_SIZE)
 
-    # `prefetch` lets the dataset fetch batches in the background while the model
-    # is training.
+    # fetch batches in the background while the model is training
     ds = ds.prefetch(buffer_size=AUTOTUNE)
 
     return ds
 
 
 train_ds = prepare_for_training(labeled_ds)
+test_ds = prepare_for_training(labeled_ds)
 
+# set variables for plot (either train or test dataset)
 image_batch, label_batch = next(iter(train_ds))
+# image_batch, label_batch = next(iter(test_ds))
 
 
+# define plots
 def show_batch(image_batch, label_batch):
     plt.figure(figsize=(15, 15))
     for n in range(25):
@@ -99,6 +105,6 @@ def show_batch(image_batch, label_batch):
 
     return plt.show()
 
+
 # show results
 show_batch(image_batch.numpy(), label_batch.numpy())
-
