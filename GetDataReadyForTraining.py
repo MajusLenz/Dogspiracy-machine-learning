abc12e2fc85dcf3e3a2355a47c6ff76717327933
Jana Borchardt <borchardt.jana@web.de>
2020-06-09 13:28:11 +0200

    ADDED model and test data set

--- GetDataReadyForTraining.py
+++ GetDataReadyForTraining.py
@@ -2,8 +2,12 @@
 # it than creates and returns batches of key-value-pairs from the processed images with their corresponding labels
 
 # TODO crop, resize, noise, rotation, etc.
+from datetime import datetime
 
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 import numpy as np
@@ -19,11 +23,11 @@ train_data_dir = cfg.train_dir
 test_data_dir = cfg.test_dir
 
 # data directory for preparing data (either train or test data)
-# data_dir = pathlib.Path(train_data_dir)
-data_dir = pathlib.Path(test_data_dir)
+train_data_dir = pathlib.Path(train_data_dir)
+test_data_dir = pathlib.Path(test_data_dir)
 
 # get possible breed names
-CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
+CLASS_NAMES = np.array([item.name for item in test_data_dir.glob('*')])
 print('breed names: ', CLASS_NAMES)
 
 # define parameters
@@ -32,9 +36,13 @@ IMG_HEIGHT = cfg.img_height
 IMG_WIDTH = cfg.img_width
 
 # create dataset of file paths
-list_ds = tf.data.Dataset.list_files(str(data_dir / '*/*.jpg'))
-for f in list_ds.take(5):
-    print('image path: ', f.numpy())
+train_list_ds = tf.data.Dataset.list_files(str(train_data_dir / '*/*.jpg'))
+test_list_ds = tf.data.Dataset.list_files(str(test_data_dir / '*/*.jpg'))
+for f in train_list_ds.take(5):
+    print('train image path: ', f.numpy())
+
+for f in test_list_ds.take(5):
+    print('test image path: ', f.numpy())
 
 
 # convert file path to an (img, label) pair
@@ -63,8 +71,14 @@ def process_path(file_path):
 
 
 # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-for image, label in labeled_ds.take(1):
+train_labeled_ds = train_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+for image, label in train_labeled_ds.take(1):
+    print("Image shape: ", image.numpy().shape)
+    print("Label: ", label.numpy())
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+test_labeled_ds = test_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+for image, label in test_labeled_ds.take(1):
     print("Image shape: ", image.numpy().shape)
     print("Label: ", label.numpy())
 
@@ -90,8 +104,8 @@ def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
     return ds
 
 
-train_ds = prepare_for_training(labeled_ds)
-test_ds = prepare_for_training(labeled_ds)
+train_ds = prepare_for_training(train_labeled_ds)
+test_ds = prepare_for_training(test_labeled_ds)
 
 # set variables for plot (either train or test dataset)
 image_batch, label_batch = next(iter(train_ds))
@@ -111,4 +125,44 @@ def show_batch(image_batch, label_batch):
 
 
 # show results
-show_batch(image_batch.numpy(), label_batch.numpy())
+# show_batch(image_batch.numpy(), label_batch.numpy())
+
+model_new = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu',
+           input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1)
+])
+
+model_new.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model_new.summary()
+
+train_image_count = len(list(train_data_dir.glob('*/*.jpg')))
+test_image_count = len(list(test_data_dir.glob('*/*.jpg')))
+
+logdir = "./logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+STEPS_PER_EPOCH = np.ceil(train_image_count / BATCH_SIZE)
+val_steps = np.ceil(test_image_count / BATCH_SIZE)
+
+model_new.fit(
+    train_ds,
+    steps_per_epoch=STEPS_PER_EPOCH,
+    epochs=1,
+    callbacks=[tensorboard_callback],
+    validation_steps= val_steps,
+    validation_data=test_ds,
+    validation_freq=10
+)
