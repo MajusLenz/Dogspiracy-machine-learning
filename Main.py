60453a9044d93cf3ffc3b3f9d7eded63b647bd64
Majus Lenz <mariuslenzing@yahoo.de>
2020-07-02 11:05:26 +0200

    Kosmetik

--- Main.py
+++ Main.py
@@ -164,21 +164,21 @@ show_batch(image_batch, label_batch)
 
 model_new = Sequential([
     Conv2D(filters=16, kernel_size=5, padding='same', activation='relu',
-           kernel_initializer="he_normal",  # good init for ReLu
+           #kernel_initializer="he_normal",  # good init for ReLu
            input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),  # add "input_shape" because this is the first layer of the Net
     MaxPooling2D(),
 Conv2D(32, 5, padding="same", activation="relu"),
         MaxPooling2D(),
         Conv2D(32, 5, padding="same", activation="relu",
-               kernel_initializer="he_normal"
+               #kernel_initializer="he_normal"
                ),
         MaxPooling2D(),
         Conv2D(64, 5, padding="same", activation="relu",
-               kernel_initializer="he_normal"
+               #kernel_initializer="he_normal"
                ),
         MaxPooling2D(),
         Conv2D(64, 5, padding="same", activation="relu",
-               kernel_initializer="he_normal"
+               #kernel_initializer="he_normal"
                ),
         MaxPooling2D(),
     Flatten(),  # transform 2D to 1D
